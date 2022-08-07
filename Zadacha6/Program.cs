1a8fa53b104c56a64596935615532e4983f7bf16
Irik <freedomirik@yandex.ru>
2022-08-07 18:26:25 +0500

    Задача №6

---
+++ Zadacha6/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Задача 6: Напишите программу, которая на вход принимает число 
+и выдаёт, является ли число чётным */
+
+Console.WriteLine("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number%2 == 0)
+{
+    System.Console.WriteLine("Число {0} четное", number);
+}
+else
+System.Console.WriteLine("Число {0} нечетное", number);
